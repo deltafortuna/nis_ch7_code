a5e282cb9d0f157a8797fd8c5f1f54ba6006b9af
Ryan J. Haasl <haaslr@uwplatt.edu>
2021-06-14 19:54:44 -0700

    Added standalone programs and R scripts.

---
+++ analyze_selective_regime.r
@@ -0,0 +1,37 @@
+analyze_selective_regime <- function(relfitAA, relfitAa, relfitaa)
+{
+	p <- seq(0,1,0.001)
+	q <- 1-p
+	relfit <- list()
+
+	relfit[[1]] <- eval(substitute(relfitAA))
+	relfit[[2]] <- eval(substitute(relfitAa))
+	relfit[[3]] <- eval(substitute(relfitaa))
+
+	wA <- p*relfit[[1]] + q*relfit[[2]]
+	wa <- p*relfit[[2]] + q*relfit[[3]]
+	wbar <- p*wA + q*wa
+
+	pt <- p * wA / wbar
+	change <- pt-p
+	vf <- data.frame("p" = p, "pdot" = change)
+	popfit <- data.frame("p" = p, "wbar" = wbar)
+	q <- list()
+	q[[1]] <- vf
+	q[[2]] <- popfit
+	return(q)
+}
+
+plot_vector_field <- function(q)
+{
+	plot(q[[1]]$p, q[[1]]$pdot, type = "l", xlab = "p", ylab = "dp/dt")
+	abline(h = 0., lty = 2)
+}
+
+plot_wbar <- function(q)
+{
+	plot(q[[2]]$p, q[[2]]$wbar, type = "l", xlab = "p", ylab = "wbar")
+	m <- q[[2]][q[[2]]$wbar == max(q[[2]]$wbar),][1]
+	print(m)
+	abline(v = m, lty = 2)
+}
---
+++ deterministic_selection.r
@@ -0,0 +1,36 @@
+deterministic_selection <- function(N, s, h, epsilon)
+{
+	timecount <- 0
+	p <- 1 / (2*N)
+	fitness <- c(1-s, 1-h*s, 1)
+	marg_fitness <- calc_marginal_fitness(fitness, p)
+
+	gen <- c(timecount)
+	frequency <- c(p)
+	popfit <- c(calc_population_fitness(p, marg_fitness))
+
+
+	while (p <= 1-epsilon) {
+		gen <- c(gen, timecount <- timecount + 1)
+		marg_fitness <- calc_marginal_fitness(fitness, p)
+		wbar <- calc_population_fitness(p, marg_fitness)
+		frequency <- c(frequency, p <- p * ( marg_fitness[2] / wbar ))
+		popfit <- c(popfit, wbar)
+	}
+
+	d <- data.frame("gen" = gen, "frequency" =  frequency, "popfit" = popfit)
+	return(d)
+}
+
+calc_marginal_fitness <- function(fitness, p)
+{
+	q <- 1-p
+	mfit <-  c(q*fitness[1] + p*fitness[2], p*fitness[3] + q*fitness[2])
+	return(mfit)
+}
+
+calc_population_fitness <- function(p, mfit)
+{
+	q <- 1-p
+	wbar <- q*mfit[1] + p*mfit[2]
+}
